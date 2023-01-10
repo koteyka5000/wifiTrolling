d4546b119160d3c693ac72c657970b06f1a778c3
koteyka5000 <romacat5000@gmail.com>
2023-01-10 23:01:23 +0300

    Почти ничего нового

--- fixPort.py
+++ fixPort.py
@@ -3,7 +3,7 @@ from os import system
 PORT = 10000
 
 def fix():
-    print(system(f'netstat -ano | findstr :{PORT}'))  # Ищем процесс, который использует наш порт
+    system(f'netstat -o | findstr :{PORT}')  # Ищем процесс, который использует наш порт
     q = input('PID-->')  # Ввести PID процесса
     system(f'taskkill -pid {q} /f')  # Завершаем этот процесс
 
